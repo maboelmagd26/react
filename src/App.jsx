35e3a91dc85e007bc635e43323edae0b30c3913c
Mahmoud Hamed Aboelmagd <m.aboelmagd@infralabs-ltd.com>
2025-07-13 03:28:48 +0300

    useStates examples mutating array, keys usage, app fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { UserProvider } from "./store/UserContext";
 import UpdateUser from "./components/UpdateUser";
+import ListSate from "./components/useState/ListSate";
+import Products from "./components/useState/Products";
 function App() {
   return (
     <UserProvider>
       <UpdateUser />
+      <Products />
     </UserProvider>
   );
 }
